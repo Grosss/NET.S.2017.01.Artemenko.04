2d0fcce3a4faaa8480f91e161d2779094ddb529e
Grosss <artemenko,nickolai@gmail.com>
2017-03-20 17:50:48 +0200

    Math.Round is added

---
+++ Task4.Tests/NewtonMethodTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Tests
+{
+    [TestFixture]
+    public class NewtonMethodTests
+    {
+        [TestCase(2.12532, 3, 0.0001, ExpectedResult = 1.2857)]
+        [TestCase(134.1324, 10, 0.000001, ExpectedResult = 1.632125)]
+        [TestCase(-12345.42512, 5, 0.00001, ExpectedResult = -6.58114)]
+        [TestCase(9, 3, 0.0001, ExpectedResult = 2.0801)]
+        public double ComputeNthRoot_PassedValidArguments_ExpectedPositiveTest(double number, int power, double eps)
+        {
+            return NewtonMethod.ComputeNthRoot(number, power, eps);
+        }
+
+        [TestCase(81, 4, 2.0001)]
+        [TestCase(-125, -3, 0.00001)]
+        public void ComputeNthRoot_PassedIncorrectEpsilonOrNegativePower_ThrowsArgumentOutOfRangeException(double number, int power, double eps)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => NewtonMethod.ComputeNthRoot(number, power, eps));
+        }
+
+        [TestCase(-16, 4, 0.00001)]
+        [TestCase(-25, 2, 0.00001)]
+        public void ComputeNthRoot_PassedNegativeNumberWithEvenPower_ThrowsArgumentException(double number, int power, double eps)
+        {
+            Assert.Throws<ArgumentException>(() => NewtonMethod.ComputeNthRoot(number, power, eps));
+        }
+    }
+}
--- Task4/NewtonMethod.cs
+++ Task4/NewtonMethod.cs
@@ -10,8 +10,9 @@ namespace Task4
     {
         public static double ComputeNthRoot(double number, int power, double eps)
         {
-            if (power < 1 || eps > 1)
+            if (power < 1 || eps > 1 || eps < 0)
                 throw new ArgumentOutOfRangeException();
+
             if (number < 0 && (power % 2 == 0))
                 throw new ArgumentException();
 
@@ -21,7 +22,13 @@ namespace Task4
                 x0 = x1;
                 x1 = 1.0 / power * ((power - 1.0) * x0 + number / Math.Pow(x0, power - 1));
             } while (Math.Abs(x1 - x0) > eps);
-            return x1;
+            int precision = 0;
+            do
+            {
+                precision++;
+                eps *= 10;
+            } while (eps < 1);
+            return Math.Round(x1, precision);
         }
     }
 }
